29afb2733c2d67e3fe2dd688f5683e278fab2358
munimrahman <munimrh@gmail.com>
2023-08-29 22:57:12 +0600

    api added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://social-server-lovat.vercel.app/api/v1',
+    baseUrl: "https://social-server-lovat.vercel.app/api/v1",
     // baseUrl: "http://localhost:5000/api/v1",
   }),
   tagTypes: [],
